e0d7d63a722fd02856c448b1c95cf8b9bb5714f6
yasmineecosta <yasmineecosta@gmail.com>
2025-03-31 18:50:20 -0300

    feat: add query to retrieve top 10 operators by annual health expenses in 2024

--- search3-5.sql
+++ search3-5.sql
@@ -17,3 +17,21 @@ WHERE UPPER(d.descricao) LIKE '%EVENTOS/SINISTROS CONHECIDOS OU AVISADOS DE ASSI
 GROUP BY d.reg_ans, o.razao_social, d.ano, d.trimestre
 ORDER BY total_despesa DESC
 LIMIT 10;
+
+-- querie para buscar os 10 maiores valores de despesas de saúde por operadora
+-- ano de 2024
+SELECT 
+    o.razao_social,
+    d.reg_ans,
+    d.ano,
+    MIN(d.data) AS data_inicial,
+    MAX(d.data) AS data_final,
+    SUM(d.vl_saldo_final) AS total_despesa
+FROM dados_trimestrais.despesas_saude d
+LEFT JOIN dados_relatorio.operadoras o 
+    ON d.reg_ans = o.registro_ans
+WHERE UPPER(d.descricao) LIKE '%EVENTOS/SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA À SAÚDE%'
+  AND d.ano = 2024
+GROUP BY d.reg_ans, o.razao_social, d.ano
+ORDER BY total_despesa DESC
+LIMIT 10;
