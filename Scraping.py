d9c07bc1554e0b1a18f67d416a14ebf0903b0fa2
yasmineecosta <yasmineecosta@gmail.com>
2025-03-31 18:16:27 -0300

    feat: download located PDF files, save locally and compress downloaded files into a zip archive

--- Scraping.py
+++ Scraping.py
@@ -29,3 +29,23 @@ for link in soup.find_all("a", href=True):
         full_url = urljoin(base_url, href)
         if full_url.endswith(".pdf"):
             pdf_links.append((text.strip(), full_url))
+
+# Faz o download dos PDFs
+downloaded_files = []
+for name, pdf_url in pdf_links:
+    filename = name.replace(" ", "_").replace("/", "_") + "pdf"
+    filepath = os.path.join(output_dir, filename)
+
+    print(f"Baixando: {pdf_url}")
+    pdf_response = requests.get(pdf_url)
+    with open(filepath, "wb") as f:
+        f.write(pdf_response.content)
+    downloaded_files.append(filepath)
+
+# Compacta os arquivos baixados em um .zip
+zip_filename = "anexos_rol_procedimentos.zip"
+with ZipFile(zip_filename, 'w') as zipf:
+    for file in downloaded_files:
+        zipf.write(file, os.path.basename(file))
+
+print(f"\nCompactação finalizada: {zip_filename}")
